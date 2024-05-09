3298c9f2498c57d148311c0641717d87c47c7f30
NSKNASIR <nsknasiruddinbhuiyan@gmail.com>
2024-05-09 21:25:47 +0600

    update1

--- main.cpp
+++ main.cpp
@@ -6,5 +6,13 @@ using namespace std;
 using json = nlohmann::json;
 
 int main(){
-    
+    ofstream ecefile("ece.json");
+    json ecedata;
+    ecedata = json::parse(R"(
+  {
+    "pi": 3.141,
+    "happy": true
+  }
+)");
+    ecefile<<ecedata;
 }
